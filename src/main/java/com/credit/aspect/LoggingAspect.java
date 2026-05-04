aa41188ee39a3ddaa2af02c476482d5cff3d6f1c
matt <6819654@gmail.com>
2026-05-04 13:53:12 +0300

    logsfinl3

--- src/main/java/com/credit/aspect/LoggingAspect.java
+++ src/main/java/com/credit/aspect/LoggingAspect.java
@@ -1,9 +1,7 @@
 package com.credit.aspect;
 
-import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
